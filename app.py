e29dd253a0658de2d5a0818c593015092d0e8d2b
rp097 <s222606849@deakin.edu.au>
2024-05-27 16:44:02 +1000

    Troubleshooting

--- app.py
+++ app.py
@@ -20,30 +20,30 @@ class_names = [
 def upload_image():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            try:
-                # Convert the FileStorage object to a byte stream
-                file_stream = io.BytesIO(file.read())
-                # Load the image from the byte stream
-                image = load_img(file_stream, target_size=(32, 32))
-                image = img_to_array(image)
-                image = np.expand_dims(image, axis=0) / 255.0
-                prediction = model.predict(image)
-                class_idx = np.argmax(prediction, axis=1)[0]
-                class_name = class_names[class_idx]
-                return redirect(url_for('show_prediction', prediction=class_name))
-            except Exception as e:
-                return redirect(url_for('show_prediction', error=str(e)))
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     if request.method == 'POST':
+#         file = request.files['file']
+#         if file:
+#             try:
+#                 # Convert the FileStorage object to a byte stream
+#                 file_stream = io.BytesIO(file.read())
+#                 # Load the image from the byte stream
+#                 image = load_img(file_stream, target_size=(32, 32))
+#                 image = img_to_array(image)
+#                 image = np.expand_dims(image, axis=0) / 255.0
+#                 prediction = model.predict(image)
+#                 class_idx = np.argmax(prediction, axis=1)[0]
+#                 class_name = class_names[class_idx]
+#                 return redirect(url_for('show_prediction', prediction=class_name))
+#             except Exception as e:
+#                 return redirect(url_for('show_prediction', error=str(e)))
 
-@app.route('/prediction')
-def show_prediction():
-    prediction = request.args.get('prediction')
-    error = request.args.get('error')
-    return render_template('prediction.html', prediction=prediction, error=error)
+# @app.route('/prediction')
+# def show_prediction():
+#     prediction = request.args.get('prediction')
+#     error = request.args.get('error')
+#     return render_template('prediction.html', prediction=prediction, error=error)
 
 if __name__ == '__main__':
     app.run(debug=True)
