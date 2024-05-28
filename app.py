daa7de5998eb5e3a8aa30d78b22a5a9a6e39e621
rp097 <s222606849@deakin.edu.au>
2024-05-28 15:04:12 +1000

    Test

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask, request, render_template, redirect, url_for
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
+#from tensorflow.keras.preprocessing.image import img_to_array, load_img
 import numpy as np
 import io
 
 app = Flask(__name__)
-model = tf.keras.models.load_model('road_sign_detector_model_1.h5')
+#model = tf.keras.models.load_model('road_sign_detector_model_1.h5')
 
-class_names = [
-    'Bus stop', 'Give way', 'Horn Prohibited', 'Hotel',
-    'Maximum speed limit (90km/h)', 'Narrow road', 'No entry',
-    'No entry for goods vehicles', 'No entry for motor vehicles',
-    'No left turn', 'No overtaking', 'No parking', 'No right turn', 'No stopping',
-    'No vehicles in both directions', 'One-way traffic', 'Parking', 'Road Hump',
-    'Turn left', 'Turn right'
-]
+# class_names = [
+#     'Bus stop', 'Give way', 'Horn Prohibited', 'Hotel',
+#     'Maximum speed limit (90km/h)', 'Narrow road', 'No entry',
+#     'No entry for goods vehicles', 'No entry for motor vehicles',
+#     'No left turn', 'No overtaking', 'No parking', 'No right turn', 'No stopping',
+#     'No vehicles in both directions', 'One-way traffic', 'Parking', 'Road Hump',
+#     'Turn left', 'Turn right'
+# ]
 
 @app.route('/')
 def upload_image():
